8d51725fe7358e57a6274683f52c27b94ce558f7
Ryan Kitty <ryan@kittydiaz.com>
2023-04-29 16:54:44 -0500

    updated arguments and added the amount used to output

--- pybill.py
+++ pybill.py
@@ -4,8 +4,10 @@ def calculate_water_bill(prev_reading, new_reading, rate):
 
     Args:
     prev_reading (float): The previous water meter reading.
-    new_reading (float): The new water meter reading.
-    rate (float): The rate per unit of water consumed.
+    new_reading (float): The current water meter reading.
+    wrate (float): The water cost per 1,000's.
+    srate (float): The sewer cost per 1,000's.
+    sratebase (float): The sewer base cost.
 
     Returns:
     float: The calculated water bill amount.
@@ -13,7 +15,7 @@ def calculate_water_bill(prev_reading, new_reading, rate):
     usage = new_reading - prev_reading
     sratecalc = usage + srate
     bill_amount = usage * wrate + sratecalc + sratebase
-    return bill_amount
+    return bill_amount, usage
 
 if __name__ == "__main__":
     print("Welcome to the Water Bill Calculator!")
@@ -22,8 +24,9 @@ if __name__ == "__main__":
     new_reading = float(input("Enter your current reading: "))
     wrate = float(input("Enter your water cost per 1,000's: "))
     srate = float(input("Enter your sewer cost per 1,000's: "))
-    sratebase = float(input("Enter your sewer cost base: "))
+    sratebase = float(input("Enter your sewer base cost: "))
     
 
-    bill_amount = calculate_water_bill(prev_reading, new_reading, wrate)
+    bill_amount, usage = calculate_water_bill(prev_reading, new_reading, wrate)
     print(f"Your water bill amount is: ${bill_amount:.2f}")
+    print(f"Water used: {usage:.2f}")
